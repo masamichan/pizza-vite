b1b35c8bffad0b60819693ef57d18e3d4b634468
masamichan <positivemasa031@gmail.com>
2023-12-18 10:46:30 +0900

    Handling Error

--- .history/src/App_20231218101833.jsx
+++ .history/src/App_20231218101833.jsx
@@ -5,7 +5,6 @@ import Cart from './features/cart/Cart';
 import CreateOrder from './features/order/CreateOrder';
 import Order from "./features/order/Order"
 import AppLayout from "./ui/AppLayout";
-import Error from "./ui/Error"
 
 const router = createBrowserRouter([
   {
@@ -13,7 +12,7 @@ const router = createBrowserRouter([
     errorElement: <Error />,
     children: [
   { path: "/", element: <Home/>},
-  { path: "/menu",element: <Menu />, loader: menuLoader, errorElement: <Error />},
+  { path: "/menu",element: <Menu />, loader: menuLoader},
   {path: "/cart",element: <Cart />},
   {path: "/order/new",element: <CreateOrder />},
   {path: "/order/:orderId",element: <Order />}
--- .history/src/ui/Error_20231218103850.jsx
+++ .history/src/ui/Error_20231218103850.jsx
@@ -8,7 +8,8 @@ function Error() {
   return (
     <div>
       <h1>Something went wrong 😢</h1>
-      <p>{error.data || error.message}</p>
+      <p>{error.data}</p>
+      <p>{error.message}</p>
       <button onClick={() => navigate(-1)}>&larr; Go back</button>
     </div>
   );
